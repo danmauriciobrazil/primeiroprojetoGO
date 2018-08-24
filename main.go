3cc8c4c7a2ce91bcb06ef3a2718ce08f233649a4
Danilo <danilo.mauricio.silva@gmail.com>
2018-08-24 18:36:57 +0000

    teste

--- main.go
+++ main.go
@@ -7,4 +7,7 @@ import (
 
 func main() {
 	fmt.Printf("Hello, world. Sqrt(2) = %v\n", math.Sqrt(2))
+	fmt.Printf("Oi mundo. Sqrt(4) = %v\n", math.Sqrt(4))
+	fmt.Printf("Hello, . Sqrt(5) = %v\n", math.Sqrt(5))
+	fmt.Printf("Hello, d. Sqrt(6) = %v\n", math.Sqrt(6))
 }
